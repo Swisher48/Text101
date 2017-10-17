04ba9d558f90b070f056c2d83dbe03ea8492f896
Swisher48 <steveholmes99@hotmail.com>
2017-10-17 20:16:25 +1100

    initial commit
    initial commit

---
+++ Assets/TextController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	private enum States {cell, mirror, sheets_0, lock_0, freedom};
+	private States myState;
+
+	// Use this for initialization
+	void Start () {
+
+		myState = States.cell;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	print (myState);
+		
+	}
+
+	void state_cell (){
+
+		text.text = "You are in a Prison Cell and want to escape. There are some dirty sheets on the bed, a mirror on the wall and the " +
+						"door is locked from the outside \n\n" +
+						"Press S to view the Sheets, M to view the Mirror and L to view the Lock ";
+
+		if (Input.GetKeyDown(KeyCode.Space)){
+
+			
+			}
+	}
+}
